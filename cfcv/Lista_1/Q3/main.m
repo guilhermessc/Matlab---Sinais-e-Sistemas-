4c2a83413766a2e723b3f11917bb8fe85cbc4754
carlos <cfcv@cin.ufpe.br>
2017-05-05 23:40:17 -0300

    Q3 bug fixed.

--- cfcv/Lista_1/Q3/main.m
+++ cfcv/Lista_1/Q3/main.m
@@ -7,23 +7,23 @@ disp('    5 - Convolução letra e');
 disp('-----------------------------');
 
 choice = input('Escolha:');
-t = [-10:1:10];
+t = [-10:0.001:10];
 
 switch choice
 	case 1
-		y = conv(u(t,0), u(t,0), 'same');
+		y = conv(heaviside(t), heaviside(t), 'same');
 	case 2
-		x = u(t,0) - u(t,-1);
-		y = conv(x,u(t,0), 'same');
+		x = heaviside(t) - heaviside(t-1);
+		y = conv(x,heaviside(t), 'same');
 	case 3
-		x = u(t,0) - u(t,-1);
+		x = heaviside(t) - heaviside(t-1);
 		y = conv(x, x, 'same'); 
 	case 4
-		x = exponencial(t,-2) .* u(t,0);
-		y = conv(u(t,0), x, 'same');
+		x = exponencial(t,-2) .* heaviside(t);
+		y = conv(heaviside(t), x, 'same');
 	case 5
-		x = exponencial(t,-1) .* u(t,0);
-		z = exponencial(t,-2) .* u(t,0);
+		x = exponencial(t,-1) .* heaviside(t);
+		z = exponencial(t,-2) .* heaviside(t);
 		y = conv(x, z, 'same'); 
 	otherwise
 		disp('Wrong choice!');
